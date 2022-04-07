e0f94ac9715074207e8e17e77379886e44efb378
Pravin.nagargoje <Pravin.Nagargoje1@T-Mobile.com>
2022-04-07 12:18:03 +0530

    Removed warnings

--- ImageListView/Utility/Common.swift
+++ ImageListView/Utility/Common.swift
@@ -9,11 +9,13 @@ import UIKit
 
 enum Alert {
     static func showAlert(_ message: String) {
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScene = scenes.first as? UIWindowScene
-        let window = windowScene?.windows.first
-        let alert = UIAlertController(title: Constants.title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: Constants.ok, style: .default, handler: nil))
-        window?.rootViewController?.present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let scenes = UIApplication.shared.connectedScenes
+            let windowScene = scenes.first as? UIWindowScene
+            let window = windowScene?.windows.first
+            let alert = UIAlertController(title: Constants.title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: Constants.ok, style: .default, handler: nil))
+            window?.rootViewController?.present(alert, animated: true, completion: nil)
+        }
     }
 }
